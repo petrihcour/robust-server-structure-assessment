cfe6b5edbd78bed675053e33ef801630f51f1f21
Vanessa <zdperfectx@gmail.com>
2023-11-20 13:30:24 -0600

    setting up folder and file structure and organizing files. psuedocode

---
+++ src/urls/urls.controller.js
@@ -0,0 +1,17 @@
+
+// GET /urls List, gets/lists all URLs 
+
+
+// POST /urls Create a new short URL
+// POST { data: {"href":"www.some-url.com"} } to /urls should assign an id to the object, save it, and return the saved object as a response to the client.
+
+
+// GET /urls/:urlId gets specific urlId 
+
+
+// PUT /urls/:urlId update specific urlId
+
+
+// DELETE /urls/:urlId deletes specific urlId
+
+
---
+++ src/urls/urls.router.js
@@ -0,0 +1,4 @@
+
+
+// List short URL uses, nested route. GET request to /urls/:urlId/uses to be inputted in here 
+
